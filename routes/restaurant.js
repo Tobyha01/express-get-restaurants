55f384d18124e9f429c396b4c02b66e7340dd5b8
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-27 15:45:40 +0000

    Added server validation to the post (create) method using 'express-validator'

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,15 +2,22 @@ const express = require("express");
 const router = express.Router()
 const seedRestaurant = require("../seedData")
 const {Restaurant} = require("../models/index")
+const {check, validationResult} = require("express-validator")
 const {sequelize} = require("../db");
 
 //TODO: Create your GET Request Route Below: 
 router.use(express.json())
 
-router.post("/", async function(request, response) {
+router.post("/", [check("name").trim().not().isEmpty(), check("location").trim().not().isEmpty(), check("cuisine").trim().not().isEmpty()], async function(request, response) {
     try{
-        const restaurant = await Restaurant.create(seedRestaurant[1])
-        response.status(200).send({restaurant})/*Returns the Restaurant object instance that was created*/
+        const errors = validationResult(request)
+        if(!errors.isEmpty()){
+            response.status(400).send(errors/* .array() */)
+        }
+        else{
+            await Restaurant.create(request.body)
+            response.status(200).send(await Restaurant.findAll())/*Returns the Restaurant object instance that was created*/
+        }
     }
     catch(error){
         response.status(500).send({error: error.message})
